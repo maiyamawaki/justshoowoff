1f8c29a8be9efec70fea4f1c6d8cd06a595d67f1
Mai Yamawaki <joooooy031006@icloud.com>
2020-12-26 15:58:02 +0900

    style modified

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,8 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 }, false);
 
+// let imgs = ["https://res.cloudinary.com/dxpxe8gus/image/upload/v1608105031/yamai/yamai-snap6.jpg.jpg",
+// "https://res.cloudinary.com/dxpxe8gus/image/upload/v1608105055/yamai/yamai-snap8.jpg.jpg"
+// ]
+
+// /*mainIndexwithoutUser*/
+// function changeImg(){
+//   let num = Math.floor(Math.random()*imgs.length);
+//   document.querySelector(".mainImg").src= imgs[num];
+// }
+
+// if(window.innerWidth>0){
+//   console.log(imgs[0])
+//   setInterval(changeImg, 2000);
+// }
+
+
 ScrollReveal().reveal('.foto',{
   scale : 0.7,
   reset : true,
   duration : 1600,
 });
+
+// ScrollReveal().reveal(".aboutYamai h1")

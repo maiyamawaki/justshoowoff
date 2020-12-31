171efc0838bc26cc47c9409f159251f6ac736edf
Mai Yamawaki <joooooy031006@icloud.com>
2020-12-31 13:35:36 +0900

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,15 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 }, false);
 
-//  document.querySelector(".mainImg").style.marginLeft = 0;
-
-// function changePosition(){
-//   document.querySelector(".mainImg").style.marginLeft ;
-//   console.log(position);
-// }
-
-// setInterval(changePosition, 1000);
-
 ScrollReveal().reveal('.foto',{
   scale : 0.7,
   reset : true,
